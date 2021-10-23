3e934cd6151441cf09219d70375565ec0b85f79d
Trung-Rei <trantrung.trantrung.tt@gmail.com>
2021-10-23 20:40:03 +0700

    small fix for web

--- static/script.js
+++ static/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
   $('form input').change(function () {
-    $('form p').text(this.files.length + " file(s) selected");
+    $('form p').text(this.files[0].name);
   });
 });
