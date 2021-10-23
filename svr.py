e9402ee9e566820be1f852a6224aaee1f417c350
Trung-Rei <trantrung.trantrung.tt@gmail.com>
2021-10-23 16:29:51 +0700

    small fix

--- svr.py
+++ svr.py
@@ -45,4 +45,5 @@ def home():
         return send_file("static/result.jpg", mimetype="image/gif")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
